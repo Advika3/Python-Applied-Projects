bf75ddc32eb99314f6efa8276bbcfdde4cc9ac0e
Advika3 <212727010+Advika3@users.noreply.github.com>
2026-03-15 16:37:56 +0530

    jpeg, png, jpg to PDF
    This script converts images from a specified folder into a single PDF file.

---
+++ Images to PDF/Images to PDF.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import os
+
+
+image_folder = "C:/Users/*YOUR NAME*/Documents/Trial"
+
+output_pdf = "C:/Users/*YOUR NAME*/Documents/Trial/images_output.pdf"
+
+images = []
+
+for file in sorted(os.listdir(image_folder)):
+    if file.lower().endswith((".png", ".jpg", ".jpeg")):
+        path = os.path.join(image_folder, file)
+        img = Image.open(path).convert("RGB")
+        images.append(img)
+
+if images:
+    images[0].save(output_pdf, save_all=True, append_images=images[1:])
+
+print("PDF created successfully!")
